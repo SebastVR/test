d48353feae31aebf89af8afeac18b83d62999f89
Sebastián Velásquez Ramírez <sebastianvr92@hotmail.com>
2020-06-04 22:14:46 -0500

    Upgrade Assignment2.py

--- Data_Science_Python/Applied_Machine_Learning_in_Python/Assignment2.py
+++ Data_Science_Python/Applied_Machine_Learning_in_Python/Assignment2.py
@@ -18,7 +18,6 @@ y = np.sin(x)+x/6 + np.random.randn(n)/10
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=0)
 
-
 # You can use this function to help you visualize the dataset by
 # plotting a scatterplot of the data points
 # in the training and test sets.
